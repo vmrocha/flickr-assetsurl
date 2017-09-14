317ea82c922959fb70a548071702529d878c2131
Vinicius de Melo Rocha <vmrocha@gmail.com>
2017-09-14 13:49:24 -0400

    Move source folder one level

---
+++ src/FlickrAssetsUrl/Configurations/FilterConfig.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace FlickrAssetsUrl.Configurations
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/FlickrAssetsUrl/Controllers/PhotosController.cs
@@ -0,0 +1,25 @@
+﻿using FlickrAssetsUrl.Models;
+using System.Web.Mvc;
+
+namespace FlickrAssetsUrl.Controllers
+{
+    public class PhotosController : BaseController
+    {
+        private string GetSearchTerm()
+        {
+            var searchTerm =
+                Request.QueryString.Count == 0 ? "" :
+                Request.QueryString[Request.QueryString.Count - 1];
+
+            if (searchTerm == "")
+                searchTerm = "Mountain";
+
+            return searchTerm;
+        }
+
+        public ActionResult Index()
+        {
+            return JsonCamelCase(FlickrProxy.SearchPhotos(GetSearchTerm()));
+        }
+    }
+}
